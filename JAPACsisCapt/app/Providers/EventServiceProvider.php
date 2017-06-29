4dc81bc6df80ba96e56a459e0f3a707181a5310a
Pedro Enriquez <pedroenrique81@gmail.com>
2017-06-28 23:58:27 -0600

    Creacion del proyecto
    Se creo el poryecto con lo siguiente:
    --Proyecto de Laravel
    --Migraciones & Relaciones
    --Integracion de AdminLTE
    --Cambio de Idioma & Zona Horaria

---
+++ JAPACsisCapt/app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace JAPACsisCapt\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'JAPACsisCapt\Events\Event' => [
+            'JAPACsisCapt\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
